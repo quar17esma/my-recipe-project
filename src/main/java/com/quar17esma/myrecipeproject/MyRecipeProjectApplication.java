8f84b382beb49d4ec772c56a770e8d904cba4ac6
Sergey Shutyi <sergey.shutyi@dataart.com>
2019-02-28 12:40:58 +0200

    Initial commit

---
+++ src/main/java/com/quar17esma/myrecipeproject/MyRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.quar17esma.myrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipeProjectApplication.class, args);
+    }
+
+}
